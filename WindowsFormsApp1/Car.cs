21d4ccedd94a1e6adc5cd432d90290d363665d5b
hoseinmohazebi <hoseinmohazebi@gmail.com>
2020-09-07 22:17:26 +0430

    Adding the Car Class

---
+++ WindowsFormsApp1/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+     class Car : Object
+    {
+        public Car()
+        {
+        }
+
+        public string Name { get; set; }
+    }
+}
